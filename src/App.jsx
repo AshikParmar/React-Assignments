8f498973ac74bc6feee5153804f31fc277543ec6
Ashik Parmar <ashikparmar5123@gmail.com>
2025-01-30 17:53:08 +0530

    Routing and Header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
+import { Outlet } from 'react-router'
 import './App.css'
 import Header from './components/Header/Header.jsx'
-import ImageGallery from './components/ImageGallery/ImageGallery.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
-      <ImageGallery />
+      <Outlet />
    </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import './Header.css'
+import { NavLink } from 'react-router'
 
 const Header = () => {
   return (
-    <div className='header'>
-      
-    </div>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <NavLink 
+                to="/" 
+                className={({ isActive }) => isActive ? 'active-link' : ''}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/gallery" 
+                className={({ isActive }) => isActive ? 'active-link' : ''}
+              >
+                Gallery
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/about" 
+                className={({ isActive }) => isActive ? 'active-link' : ''}
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
   )
 }
 
